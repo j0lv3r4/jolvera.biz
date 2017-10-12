9109de00c0f054e4f08534c97a7de8627c9e9503
Juan Olvera <jolvera@posteo.de>
2017-10-12 18:54:52 -0500

    changes on layout

--- public/js/coinclap.js
+++ public/js/coinclap.js
@@ -1,42 +1,44 @@
 function CoinClap(options) {
-    this.target = options.target || document.body;
-    this.color = options.color || 'black';
-    this.siteKey = options.siteKey || '';
+    this.miner = options.miner;
+    this.color = options.color || 'light';
+    this.animationClass = this.color === 'light'
+        ? '_coinclap-pulse-light'
+        : '_coinclap-pulse-dark';
+    this.imageSource = options.imageSource || 'https://image.flaticon.com/icons/svg/109/109638.svg';
+    this.target = document.body;
     this.button = document.createElement('button');
     this.image = document.createElement('img');
 }
 
 CoinClap.prototype.init = function() {
+    if (this.miner === undefined) {
+        throw 'Could not use CoinHive. Maybe the user is running an adBlocker.';
+    }
     this.button.setAttribute('id', '_coinclap-clap-button');
     this.button.setAttribute('class', '_coinclap-animated');
     this.image.setAttribute('id', '_coinclap-clap-img');
-    this.image.setAttribute('src', 'https://image.flaticon.com/icons/svg/109/109638.svg');
+    this.image.setAttribute('src', this.imageSource);
     this.image.setAttribute('draggable', false);
     this.button.appendChild(this.image);
     this.target.appendChild(this.button);
 
-    this.miner = new CoinHive.Anonymous(this.siteKey);
     this.bindUIEvents();
 }
 
 CoinClap.prototype.bindUIEvents = function() {
-    document.getElementById('_coinclap-clap-button').addEventListener('mousedown', function() {
-        this.miner.start();
-        this.addAnimationClass();
-    }.bind(this));
+    document.getElementById('_coinclap-clap-button').addEventListener('mousedown', this.minerStart.bind(this));
 
-    document.getElementById('_coinclap-clap-button').addEventListener('mouseup', function() {
-        this.miner.stop();
-        this.removeAnimationClass();
-    }.bind(this));
+    document.getElementById('_coinclap-clap-button').addEventListener('mouseup', this.minerStop.bind(this));
 
-    document.getElementById('_coinclap-clap-button').addEventListener('mouseout', this.removeAnimationClass);
+    document.getElementById('_coinclap-clap-button').addEventListener('mouseleave', this.minerStop.bind(this));
 }
 
-CoinClap.prototype.addAnimationClass = function() {
-    document.getElementById('_coinclap-clap-button').classList.add('_coinclap-pulse');
+CoinClap.prototype.minerStart = function() {
+    this.miner.start();
+    document.getElementById('_coinclap-clap-button').classList.add(this.animationClass);
 }
 
-CoinClap.prototype.removeAnimationClass = function(event) {
-   document.getElementById('_coinclap-clap-button').classList.remove('_coinclap-pulse');
+CoinClap.prototype.minerStop = function(event) {
+   this.miner.stop();
+   document.getElementById('_coinclap-clap-button').classList.remove(this.animationClass);
 }
